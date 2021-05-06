c99ac1926fc25c00f90c4d4894a754c8ec33db71
Rafael Lucio <rafaellucio@mbrafael.local>
2021-05-06 13:00:37 -0300

    endpoint register person

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,12 @@
 import datetime
+from datetime import datetime
+import json
 import os
+from uuid import uuid4
 
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 from flask_mongoengine import MongoEngine
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
@@ -15,20 +19,30 @@ app.config['MONGODB_SETTINGS'] = {
 db = MongoEngine()
 db.init_app(app)
 
-class Todo(db.Document):
-    title = db.StringField(max_length=60)
-    text = db.StringField()
-    done = db.BooleanField(default=False)
-    pub_date = db.DateTimeField(default=datetime.datetime.now)
 
-@app.route("/api")
-def index():
-    Todo.objects().delete()
-    Todo(title="Simple todo A", text="12345678910").save()
-    Todo(title="Simple todo B", text="12345678910").save()
-    Todo.objects(title__contains="B").update(set__text="Hello world")
-    todos = Todo.objects().to_json()
-    return Response(todos, mimetype="application/json", status=200)
+class Person(db.Document):
+    first_name = db.StringField(max_length=50)
+    last_name = db.StringField(max_length=50)
+    birthday = db.StringField()
+    password = db.StringField(max_length=200)
+    username = db.StringField(max_length=200)
+    user_id = db.UUIDField(binary=False, default=uuid4(), required=True)
+
+
+@app.route("/account/person", methods=["POST"])
+def save():
+    data = json.loads(request.data)
+    person = Person(first_name=data['first_name'], last_name=data['last_name'],
+                    birthday=data['birthday'],
+                    password=generate_password_hash(data['password']), username=data['username']).save()
+    return jsonify(person)
+
+
+@app.route("/account/friends")
+def list():
+    people = Person.objects().to_json()
+    return Response(people, mimetype="application/json", status=200)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
