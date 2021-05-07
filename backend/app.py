d5dee117b5702f3366ce06bca65eaa46f628ed6d
Rafael Lucio <rafaellucio@mbrafael.local>
2021-05-07 14:30:14 -0300

    final adjustments

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import os
 from uuid import uuid4
@@ -24,7 +25,7 @@ class Person(db.Document):
     birthday = db.StringField()
     password = db.StringField()
     username = db.StringField()
-    user_id = db.UUIDField(binary=False, default=uuid4(), required=True)
+    user_id = db.StringField(default=str(uuid4()))
 
 
 class BillingCard(db.Document):
@@ -34,23 +35,37 @@ class BillingCard(db.Document):
     expiry_yyyy = db.StringField()
     security_code = db.StringField()
     date = db.StringField()
-    card_id = db.UUIDField(binary=False, default=uuid4(), required=True)
+    card_id = db.StringField(default=str(uuid4()))
 
 
 class Transfer(db.Document):
     user_id = db.StringField()
     friend_id = db.StringField()
-    total_to_pay = db.IntField()
-    billing_card = BillingCard
+    total_to_transfer = db.IntField()
+    date = db.DateTimeField(default=datetime.date.today())
+    billing_card = db.StringField()
+
+
+class TransferDto:
+    def __init__(self, user_id, friend_id, value, date, from_card):
+        self.user_id = user_id
+        self.friend_id = friend_id
+        self.value = value
+        self.date = date
+        self.from_card = from_card
 
 
 @app.route("/account/person", methods=["POST"])
 def save_person():
-    data = json.loads(request.data)
-    person = Person(first_name=data['first_name'], last_name=data['last_name'],
-                    birthday=data['birthday'],
-                    password=generate_password_hash(data['password']), username=data['username'])
-    person.save()
+    try:
+        data = json.loads(request.data)
+        person = Person(first_name=data['first_name'], last_name=data['last_name'],
+                        birthday=data['birthday'],
+                        password=generate_password_hash(data['password']), username=data['username'])
+        person.save()
+    except:
+        return Response('Error when registering person', mimetype="application/json", status=400)
+
     return Response('Successfully created person ', mimetype="application/json", status=200)
 
 
@@ -62,11 +77,15 @@ def list_friends():
 
 @app.route("/account/card", methods=["POST"])
 def save_card():
-    data = json.loads(request.data)
-    card = BillingCard(title=data['title'], pan=data['pan'],
-                       expiry_mm=data['expiry_mm'], expiry_yyyy=data['expiry_yyyy'],
-                       security_code=data['security_code'], date=data['date'])
-    card.save()
+    try:
+        data = json.loads(request.data)
+        card = BillingCard(title=data['title'], pan=data['pan'],
+                           expiry_mm=data['expiry_mm'], expiry_yyyy=data['expiry_yyyy'],
+                           security_code=data['security_code'], date=data['date'])
+        card.save()
+    except:
+        return Response('Error when registering billing card', mimetype="application/json", status=400)
+
     return Response('Successfully created billing card', mimetype="application/json", status=200)
 
 
@@ -74,6 +93,39 @@ def save_card():
 def list_cards():
     cards = BillingCard.objects().to_json()
     return Response(cards, mimetype="application/json", status=200)
+
+
+@app.route("/account/transfer", methods=["POST"])
+def save_transfer():
+    try:
+        data = json.loads(request.data)
+        card = BillingCard.objects(card_id=data['billing_card']['card_id']).first()
+        transfer = Transfer(friend_id=data['friend_id'], user_id=data['user_id'],
+                            billing_card=card['card_id'],
+                            total_to_transfer=data['total_to_transfer'])
+        transfer.save()
+    except:
+        return Response('Error when performing transfer', mimetype="application/json", status=400)
+
+    return Response('Successfully transfer', mimetype="application/json", status=200)
+
+
+@app.route("/account/bank-statement")
+def list_bank_statemens():
+    list_statements = []
+    for bank_statement in Transfer.objects:
+        list_statements.append(TransferDto(bank_statement['user_id'], bank_statement['friend_id'],
+                                           bank_statement['total_to_transfer'],
+                                           bank_statement['date'].strftime('%d/%m/%Y'),
+                                           bank_statement['billing_card']))
+
+    return Response(json.dumps([ob.__dict__ for ob in list_statements]), mimetype="application/json", status=200)
+
+
+@app.route("/account/bank-statement/<string:userid>")
+def list_bank_statemens_by_user(userid):
+    bank_statements = Transfer.objects(user_id=userid).to_json()
+    return Response(bank_statements, mimetype="application/json", status=200)
 
 
 if __name__ == "__main__":
